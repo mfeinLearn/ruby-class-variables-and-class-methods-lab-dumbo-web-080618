b5b1bb4a0b65e11ce4ca5c88abd7ef83f5a5f43c
Malcome Fein <mfein90@gmail.com>
2018-08-13 12:20:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,22 @@
+class Song
+	attr_accessor :name, :artists, :genre 
+	@@count = 0
+	@@artists = []
+
+	def initialize(name, artists, genre)
+		@name = name
+		@@artists 
+		@genre = genre
+		@@count += 1
+
+	end
+
+	def self.count
+		@@count
+	end
+	
+	def self.artists
+	  @@artists
+	end
+end
 
